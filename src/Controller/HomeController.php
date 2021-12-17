c2a1200480cb9b5e506956b2fee1d9df048b0c3d
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2021-12-17 19:25:32 +0200

    Controller + view

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(path: '/', name: 'homepage')]
+    public function __invoke(): Response
+    {
+        return $this->render('homepage.html.twig');
+    }
+}
